3577f72370b0c9acbac01640c63597a208cba9c6
kacpi52 <kacperkrajewski52@gmail.com>
2022-01-13 17:06:12 +0100

    search components

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{ backgroundColor: "#eee" , flex:1,}}>
+            <View style={{ backgroundColor: "white" , padding:15, }}> 
+                <HeaderTabs  />
+                <SearchBar />
+            </View>
+            
+        </SafeAreaView>
+    )
+}
+
+export default Home
