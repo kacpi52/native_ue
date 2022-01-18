2ec5f3848c782a40c1b671c7b80e64fdbd404c78
kacpi52 <kacperkrajewski52@gmail.com>
2022-01-18 22:04:37 +0100

    cd

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
-import RestaurantItem from '../components/RestaurantItem'
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
 import SearchBar from '../components/SearchBar'
 
 const Home = () => {
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
     return (
         <SafeAreaView style={{ backgroundColor: "#eee" , flex:1,}}>
             <View style={{ backgroundColor: "white" , padding:15, }}> 
                 <HeaderTabs  />
                 <SearchBar />
             </View>
-            <ScrollView>
+            <ScrollView showsVerticalScrollIndicator={false} >
                 <Categories />
-                <RestaurantItem />
+                <RestaurantItems restaurantData={restaurantData} />
             </ScrollView>
         </SafeAreaView>
     )
